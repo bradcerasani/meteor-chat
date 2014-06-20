1a118ec43dea5a9c467bc568f45484a051e60d0a
Brad Cerasani <bradcerasani@gmail.com>
2014-06-19 23:30:14 -0500

    added sending messages functionality

--- client/client.js
+++ client/client.js
@@ -1,3 +1,23 @@
 Template.messages.messages = function() {
   return Messages.find({}, {sort: {time: -1}});
 }
+
+Template.input.events = {
+  'keydown input#message' : function(e) {
+    if (e.which == 13) {
+      var name = 'Anonymous';
+      var message = document.getElementById('message');
+
+      if (message.value != '') {
+        Messages.insert({
+          name: name,
+          message: message.value,
+          time: Date.now(),
+        });
+
+        document.getElementById('message').value = '';
+        message.value = '';
+      }
+    }
+  }
+}
